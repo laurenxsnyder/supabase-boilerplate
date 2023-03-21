873d3e105f17ebd1749b1328883d93bd13968fa8
Lauren Snyder <laurenxsnyder@gmail.com>
2023-03-20 22:25:40 -0400

    add books and files

---
+++ index.js
@@ -0,0 +1,18 @@
+import { createClient } from "https://cdn.skypack.dev/@supabase/supabase-js@2.10.0"
+
+const supabaseUrl = 'https://dfbjbyjzsfrypaymxtau.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRmYmpieWp6c2ZyeXBheW14dGF1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg4MjU0NjcsImV4cCI6MTk5NDQwMTQ2N30.YopGD6EjkM7QL8kgYe4H98geE2YjFztiTj65MhpQ3Dc'
+const supabase = createClient(supabaseUrl, supabaseKey)
+
+async function getBooks() {
+  let { data: books, error } = await supabase
+  .from('books')
+  .select('*')
+
+for (let book of books) {
+  let bookList = document.getElementById('books');
+  bookList.innerHTML+=`<li>${book.title}</li>` ;
+}
+}
+
+getBooks();
